3f1b3c83eec0d8204a918a8b4842b5cbe7416c69
Shashy Dass <ShashyD@gmail.com>
2013-04-26 10:26:20 -0400

    setup directories and project.clj files

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject hs4clj "0.1.0-SNAPSHOT"
+  :description "Clojure wrapper around hs4j providing java handler socket client"
+  :url "https://github.com/esdee/hs4clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [mysql/mysql-connector-java "5.1.6"]])
---
+++ src/hs4clj/core.clj
@@ -0,0 +1,6 @@
+(ns hs4clj.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
